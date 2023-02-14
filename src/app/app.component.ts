7568a81b13819551afbd86c244866d247045cef3
kaminari0206 <y1281820@gmail.com>
2023-02-14 15:31:49 -0500

    add todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    {description: 'eat', done: false},
+    {description: 'sleep', done: false},
+    {description: 'play', done: false},
+    {description: 'laugh', done: false},
+  ];
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item)=> this.filter === 'done' ? item.done : !item.done);
+  }
 }
